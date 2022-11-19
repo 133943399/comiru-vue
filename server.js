12e6fb1764ad811b7ac43437a6c165cbdf06899a
Liii <133943399@qq.com>
2022-11-19 13:00:45 +0800

    update

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
 const express = require('express');
+const serveStatic = require("serve-static")
 const path = require('path');
-const history = require('connect-history-api-fallback');
-
-const app = express();
-
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
-
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-app.use(staticFileMiddleware);
-
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
 app.route('/*')
     .get(function(req, res) {
           res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-// app.get('/', function (req, res) {
-//   res.render(path.join(__dirname + '/dist/index.html'));
-// });
-
-var server = app.listen(80, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+const port = process.env.PORT || 80;
+app.listen(port);
